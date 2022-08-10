685c6fbe17fe40d74de63c7f1735aff474c29aae
taradhita <taradhita@gmail.com>
2022-08-10 21:35:01 +0800

    exercise 3.16

--- src/index.js
+++ src/index.js
@@ -84,6 +84,26 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
