f8618340f56087fd507a9a46c8c263ba2aa80035
taradhita <taradhita@gmail.com>
2022-08-10 21:16:55 +0800

    exercise 3.15

--- src/index.js
+++ src/index.js
@@ -35,50 +35,53 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-    const {name, number} = request.body
+  const {name, number} = request.body
 
-    if (!name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
-
-    if (!number) {
-      return response.status(400).json({ 
-        error: 'number missing' 
-      })
-    }
-
-    // if (persons.findIndex(person => person.name.toLowerCase() === name.toLowerCase()) !== -1) {
-    //   return response.status(400).json({ 
-    //     error: 'name must be unique' 
-    //   })
-    // }
-
-    const person = new Person({
-        name: name,
-        number: number,
+  if (!name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
     })
+  }
 
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
+  if (!number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
     })
-    
+  }
 
+  // if (persons.findIndex(person => person.name.toLowerCase() === name.toLowerCase()) !== -1) {
+  //   return response.status(400).json({ 
+  //     error: 'name must be unique' 
+  //   })
+  // }
+
+  const person = new Person({
+      name: name,
+      number: number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const {id} = request.params
-    Person.findById(id).then(person => {
-      response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+  const {id} = request.params
+  Person.findById(id).then(person => {
+    if (!person) {
+      response.status(404).end()
+    }
+    response.json(person)
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const {id} = request.params
-    persons = persons.filter(person => person.id !== Number(id))
-
-    response.status(204).end()
+  const {id} = request.params
+  Person.findByIdAndRemove(id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
